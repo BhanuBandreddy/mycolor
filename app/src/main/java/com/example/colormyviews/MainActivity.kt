b498c4fb8c6bcf35e0777941c700125e43662f66
Bhanuharish <hareesh.b3@gmail.com>
2022-04-15 11:14:02 +0530

    thank you

--- app/src/main/java/com/example/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.colormyviews
 import android.graphics.Color
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -21,10 +22,15 @@ class MainActivity : AppCompatActivity() {
         val boxThreeText = findViewById<TextView>(R.id.box_three_text)
         val boxFourText = findViewById<TextView>(R.id.box_four_text)
         val boxFiveText = findViewById<TextView>(R.id.box_five_text)
+        val redButton = findViewById<Button>(R.id.red_button)
+        val greenButton = findViewById<Button>(R.id.green_button)
+        val yellowButton = findViewById<Button>(R.id.yellow_button)
+
 
         val clickableViews: List<View> =
             listOf(boxOneText, boxTwoText, boxThreeText,
-                boxFourText, boxFiveText)
+                boxFourText, boxFiveText,yellowButton,redButton,greenButton)
+
         for (item in clickableViews) {
             item.setOnClickListener{makeColored(it)}
         }
@@ -41,7 +47,9 @@ class MainActivity : AppCompatActivity() {
             R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_light)
             R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
             R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_green_light)
-
+            R.id.red_button-> boxThreeText.setBackgroundResource(R.color.my_red)
+            yellow_button -> boxFourText.setBackgroundResource(R.color.my_yellow)
+            green_button -> boxFiveText.setBackgroundResource(R.color.my_green)
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
     }
